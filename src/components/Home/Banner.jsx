e11cbdd139e3297d65b9e0d6abf7f759ca3ef88c
Avinash01 <avinash.m1302@gmail.com>
2024-06-10 23:55:27 +0530

    added footer

---
+++ src/components/Home/Banner.jsx
@@ -0,0 +1,177 @@
+import Slider from "react-slick";
+
+import {
+  bannerImg1,
+  bannerImg2,
+  bannerImg3,
+  bannerImg4,
+  bannerImg5,
+} from "../../assets/images";
+
+import { useState } from "react";
+
+const Banner = () => {
+  const [dotActive, setDocActive] = useState(0);
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    beforeChange: (prev, next) => {
+      setDocActive(next);
+    },
+    appendDots: (dots) => (
+      <div
+        style={{
+          position: "absolute",
+          top: "70%",
+          left: "50%",
+          transform: "translate(-50%, 0)",
+          width: "210px",
+        }}
+      >
+        <ul
+          style={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          {dots}
+        </ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div
+        style={
+          i === dotActive
+            ? {
+                width: "30px",
+                height: "30px",
+                borderRadius: "50%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                color: "white",
+                background: "#131921",
+                padding: "8px 0",
+                cursor: "pointer",
+                border: "1px solid #f3a847",
+              }
+            : {
+                width: "30px",
+                height: "30px",
+                borderRadius: "50%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                background: "#232F3E",
+                color: "white",
+                padding: "8px 0",
+                cursor: "pointer",
+                border: "1px solid white",
+              }
+        }
+      >
+        {i + 1}
+      </div>
+    ),
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          dots: true,
+          appendDots: (dots) => (
+            <div
+              style={{
+                position: "absolute",
+                top: "60%",
+                left: "50%",
+                transform: "translate(-50%, 0)",
+                width: "150px",
+              }}
+            >
+              <ul
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  fontSize: "12px",
+                }}
+              >
+                {" "}
+                {dots}{" "}
+              </ul>
+            </div>
+          ),
+          customPaging: (i) => (
+            <div
+              style={
+                i === dotActive
+                  ? {
+                      width: "25px",
+                      height: "25px",
+                      borderRadius: "50%",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      color: "white",
+                      background: "#131921",
+                      padding: "8px 0",
+                      cursor: "pointer",
+                      border: "1px solid #f3a847",
+                    }
+                  : {
+                      width: "25px",
+                      height: "25px",
+                      borderRadius: "50%",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      background: "#232F3E",
+                      color: "white",
+                      padding: "8px 0",
+                      cursor: "pointer",
+                      border: "1px solid white",
+                    }
+              }
+            >
+              0{i + 1}
+            </div>
+          ),
+        },
+      },
+    ],
+  };
+  return (
+    <div className="w-full">
+      <div className="w-full h-full relative">
+        <Slider {...settings}>
+          <div>
+            <img src={bannerImg1} alt="" />
+          </div>
+          <div>
+            <img src={bannerImg2} alt="" />
+          </div>
+          <div>
+            <img src={bannerImg3} alt="" />
+          </div>
+          <div>
+            <img src={bannerImg4} alt="" />
+          </div>
+          <div>
+            <img src={bannerImg5} alt="" />
+          </div>
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
+
+
